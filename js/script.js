d695e38000eabb65d4617300b5603a5e57aee36f
Elaine Delgado <delgado.elaine03@gmail.com>
2021-03-25 18:52:34 -0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const btnMenu = document.querySelector(".fa-bars")
+const mobileMenu = document.querySelector(".nav-bar")
+
+
+function handleClick(event) {
+  event.preventDefault()
+  mobileMenu.classList.toggle('active')
+}
+btnMenu.addEventListener('click', handleClick)
